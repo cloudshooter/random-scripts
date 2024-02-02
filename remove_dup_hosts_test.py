54212893c35e89a6249213cdd9b1cb6ea197fc20
cloudshooter <cloudshooter@gmail.com>
2024-02-02 12:43:36 -0600

    Update remove_dup_hosts_test.py

--- remove_dup_hosts_test.py
+++ remove_dup_hosts_test.py
@@ -14,12 +14,8 @@ def process_file(input_file, output_file):
             file.write(cleaned_value + '\n')
             count += 1
             # Insert separator after every 1/3 of the values, excluding the last iteration
-            if count % step == 0 and count < len(host_set) and index < len(host_set) - 1:
+            if count % step == 0 and count < len(host_set) and count < step * 2:
                 file.write("===========\n")
-
-        # Insert separator if there are more than two sections
-        if len(host_set) // step > 2:
-            file.write("===========\n")
 
     # Print the number of items in the list divided by 3
     print(f"Number of items in the list divided by 3: {len(host_set) // 3}")
